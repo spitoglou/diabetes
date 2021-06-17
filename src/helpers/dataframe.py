e4609006c35cdfdb5de46f944e3777f852925a66
Stavros Pitoglou <s.pitoglou@csl.gr>
2021-06-17 11:27:12 +0300

    Add logging to read and save df

--- src/helpers/dataframe.py
+++ src/helpers/dataframe.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from loguru import logger
 
 
 def save_df(dataframe, filename='test.pkl'):
+    logger.info(f'Attempting to save file {filename}')
     dataframe.to_pickle(filename)
 
 
 def read_df(filename='test.pkl'):
+    logger.info(f'Attempting to read from pickle file {filename}')
     return pd.read_pickle(filename)
