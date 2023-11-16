b852ca3fb59d77bd770b48f609779c24294e927e
Stavros Pitoglou <s.pitoglou@csl.gr>
2023-11-16 08:57:50 +0200

    clean up

---
+++ dataset_generator.py
@@ -0,0 +1,27 @@
+from src.helpers.experiment import create_tsfresh_dataframe
+import itertools
+
+patients = [559, 563, 570, 575, 588, 591]
+windows = [6, 12]
+horizons = [1, 6, 12]
+
+for patient, window, horizon in itertools.product(patients, windows, horizons):
+    parameters = {
+        "ohio_no": patient,
+        "scope": "train",
+        "train_ds_size": 0,
+        "window_size": window,
+        "prediction_horizon": horizon,
+        "minimal_features": False,
+    }
+    create_tsfresh_dataframe(parameters)
+
+    parameters = {
+        "ohio_no": patient,
+        "scope": "test",
+        "train_ds_size": 0,
+        "window_size": window,
+        "prediction_horizon": horizon,
+        "minimal_features": False,
+    }
+    create_tsfresh_dataframe(parameters)
