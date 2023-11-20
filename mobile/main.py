13b7284fba29d89e45c676a2c67f1c66cb9f73a8
Stavros Pitoglou <s.pitoglou@csl.gr>
2023-11-20 17:09:01 +0200

    Streamsync working

--- mobile/main.py
+++ mobile/main.py
@@ -3,6 +3,7 @@ import config.mongo_config as mg_conf
 from src.mongo import MongoDB
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 # This is a placeholder to get you started or refresh your memory.
 # Delete it or adapt it as necessary.
@@ -25,24 +26,51 @@ def _retrieve_data(limit: int = 50):
     measurements_df = pd.DataFrame(
         list(measurements_collection.find(limit=limit, sort=[("_id", -1)]))
     )
-    measurements_df['_id'] = measurements_df['_id'].astype(pd.StringDtype())
+    measurements_df["_id"] = measurements_df["_id"].astype(pd.StringDtype())
+    measurements_df["date_time"] = pd.to_datetime(
+        measurements_df["effectiveDateTime"]
+    ).astype(str)
+    measurements_df["value"] = None
+    for index, row in measurements_df.iterrows():
+        print(row["valueQuantity"]["value"])
+        measurements_df.at[index, "value"] = row["valueQuantity"]["value"]
+        # row['value'] = row['valueQuantity']['value']
 
     predictions_df = pd.DataFrame(
         list(predictions_collection.find(limit=limit, sort=[("_id", -1)]))
     )
-    predictions_df['_id'] = predictions_df['_id'].astype(pd.StringDtype())
-    predictions_df['prediction_origin_time'] = pd.to_datetime(predictions_df['prediction_origin_time']).astype(str)
-    predictions_df['prediction_time'] = pd.to_datetime(predictions_df['prediction_time']).astype(str)
+    predictions_df["_id"] = predictions_df["_id"].astype(pd.StringDtype())
+    predictions_df["prediction_origin_time"] = pd.to_datetime(
+        predictions_df["prediction_origin_time"]
+    ).astype(str)
+    predictions_df["prediction_time"] = pd.to_datetime(
+        predictions_df["prediction_time"]
+    ).astype(str)
 
     return measurements_df, predictions_df
 
 
-def _create_temp_graph(dataframe, low, high):
-    temp_graph = px.scatter(
-        dataframe,
-        x="timestamp",
+def _create_graph(measurements, predictions, low, high):
+    measurements = measurements.sort_values(by="date_time")
+    predictions = predictions.sort_values(by="prediction_time")
+
+    temp_graph = px.line(
+        measurements,
+        x="date_time",
         y="value",
         # ?title='Room Temp'
+    ).update_layout(yaxis={'range': [0, 450]})
+
+    temp_graph.add_traces(
+        list(
+            px.line(
+                predictions,
+                x="prediction_time",
+                y="prediction_value",
+                range_y=[0,450]
+                # ?title='Room Temp'
+            ).update_traces(line_color='red').select_traces()
+        )
     )
     # Just add lines
     # temp_graph.add_hline(y=TEMP_LOW_LIMIT)
@@ -80,8 +108,10 @@ def increment(state):
 
 def refresh(state):
     measurements, predictions = _retrieve_data()
-    state['data']['measurements'] = measurements
-    state['data']['predictions'] = predictions
+    state["data"]["measurements"] = measurements
+    state["data"]["predictions"] = predictions
+    state["graph"] = _create_graph(measurements, predictions, 80, 140)
+
 
 # Initialise the state
 
@@ -101,7 +131,8 @@ initial_state = ss.init_state(
         "data": {
             "measurements": meas_df,
             "predictions": pred_df,
-        }
+        },
+        "graph": None,
     }
 )
 
